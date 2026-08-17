d11a3fcf61d8128e6b9328b0d849bcdb08b26eb4
kaua212106 <hkaua7539@gmail.com>
2026-08-17 10:12:14 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+window.CENTRAL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyA8zLyzYwRv3qDIw-8H4_Tesy8iiH1haaA",
+  authDomain: "central-de-apps.firebaseapp.com",
+  projectId: "central-de-apps",
+  storageBucket: "central-de-apps.firebasestorage.app",
+  messagingSenderId: "222066712643",
+  appId: "1:222066712643:web:130c3d5ebc5c4b935d74f6",
+  measurementId: "G-44P6G2ZSE3"
+};
+
+window.CENTRAL_APP_CHECK_SITE_KEY = "";
